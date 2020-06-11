27835e27d58029e9c22b1d854724fe42d2fa16d2
Ernest <terminat12@gmail.com>
2020-06-11 12:27:50 +0200

    scss added

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,26 @@
+// plugins
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import scss from 'rollup-plugin-scss';
 
-// `npm run build` -> `production` is true
-// `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
 	input: 'src/main.js',
 	output: {
 		file: 'public/bundle.js',
-		format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+		format: 'iife',
 		sourcemap: true
 	},
 	plugins: [
-		resolve(), // tells Rollup how to find date-fns in node_modules
-		commonjs(), // converts date-fns to ES modules
-		production && terser() // minify, but only in production
+		resolve(), 
+		commonjs(),
+		production && terser(),
+		scss({
+			output: true,
+			output: 'public/bundle.css',
+			watch: 'src/styles',
+		}),
 	]
 };
