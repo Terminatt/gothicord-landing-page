5c9ae37918aaeacd8e35cf5e3f43dd41c597309f
Ernest <terminat12@gmail.com>
2020-06-28 20:15:34 +0200

    navigation is hidden on smaller screen

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './styles/global.scss'
   // utils
   import "./styles/utils//mixins.scss";
   import "./styles/utils/variables.scss";
+  import "./styles/utils/breakpoints.scss";
 
   // components
   import "./styles/components/header.scss";
