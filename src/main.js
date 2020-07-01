5aaa2dbb0d9d7af6924cbda48f4cec17b8029150
Ernest <terminat12@gmail.com>
2020-07-01 20:29:56 +0200

    swiper added

--- src/main.js
+++ src/main.js
@@ -25,11 +25,30 @@ import "./styles/sections/cards.scss";
 // jQuery
 import $ from "jquery";
 
+// addons
+import Swiper from 'swiper';
+
 
 
 
 $(document).ready(function () {
+  const mySwiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 100,
+    grabCursor: true,
+    slidesPerView: 3,
+    centeredSlides: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
   attachHamburgerHandler(expandNavMobileMenu);
+
 })
 
 function expandNavMobileMenu() {
